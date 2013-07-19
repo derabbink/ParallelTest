90143c8b560a5d68f5f9ba7dd46380140bd71888
Maarten Abbink <m.abbink@fugro.nl>
2013-07-19 15:33:49 +0200

    extracted parallel execution bits from ParallelInstruction2

---
+++ Parallel.Coordinator/Instruction/ParallelInstruction.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Parallel.Coordinator.Interface.Instruction;
+using Parallel.Worker.Interface;
+
+namespace Parallel.Coordinator.Instruction
+{
+    internal static class ParallelInstruction
+    {
+        internal static Task RunInstruction<TArgument, TResult>(CoordinatedInstruction<TArgument, TResult> instruction,
+                                                                 Action progress, TArgument argument,
+                                                                 CancellationTokenSource ownCts,
+                                                                 IEnumerable<CancellationTokenSource> otherCts,
+                                                                 Action<TaskCanceledException>
+                                                                     handleCancellationException,
+                                                                 Action<Exception> handleOperationalException,
+                                                                 Action<Future<TResult>> handleFuture)
+            where TArgument : class
+            where TResult : class
+        {
+            Task task = Task.Factory.StartNew(() =>
+                {
+                    progress();
+                    try
+                    {
+                        var future = instruction.InvokeAndWait(ownCts.Token, progress, argument);
+                        handleFuture(future);
+                    }
+                    catch (AggregateException e)
+                    {
+                        if (e.InnerException is TaskCanceledException)
+                            handleCancellationException(e.InnerException as TaskCanceledException);
+                        else
+                            handleOperationalException(e.InnerException);
+                        CancelAllButOne(ownCts, otherCts);
+                    }
+                });
+            return task;
+        }
+
+        internal static void ProcessParallelExecutionErrors(ConcurrentQueue<Exception> operationalExceptions,
+                                                            ConcurrentQueue<TaskCanceledException>
+                                                                cancellationExceptions,
+                                                            CancellationToken cancellationToken)
+        {
+            if (operationalExceptions.Any())
+                throw operationalExceptions.First();
+            if (cancellationExceptions.Any())
+            {
+                if (cancellationToken.IsCancellationRequested)
+                    cancellationToken.ThrowIfCancellationRequested();
+                else
+                    throw cancellationExceptions.First();
+            }
+        }
+
+        internal static CancellationTokenSource[] GenerateCancellationTokenSources(int count)
+        {
+            return Enumerable.Repeat<object>(null, count).Select(_ => new CancellationTokenSource()).ToArray();
+        }
+
+        private static void CancelAllButOne(CancellationTokenSource one, IEnumerable<CancellationTokenSource> all)
+        {
+            CancelAll(all.Where(a => a != one));
+        }
+
+        internal static void CancelAll(IEnumerable<CancellationTokenSource> toCancel)
+        {
+            foreach (var cts in toCancel)
+                cts.Cancel();
+        }
+    }
+}
--- Parallel.Coordinator/Instruction/ParallelInstruction2.cs
+++ Parallel.Coordinator/Instruction/ParallelInstruction2.cs
@@ -43,74 +43,31 @@ namespace Parallel.Coordinator.Instruction
 
             Func<CancellationToken, Action, TArgument, Tuple<TResult1, TResult2>> result = (ct, progress, arg) =>
                 {
-                    CancellationTokenSource cts1 = new CancellationTokenSource();
-                    CancellationTokenSource cts2 = new CancellationTokenSource();
                     ConcurrentQueue<Exception> operationalExceptions = new ConcurrentQueue<Exception>();
                     ConcurrentQueue<TaskCanceledException> cancellationExceptions = new ConcurrentQueue<TaskCanceledException>();
+
+                    CancellationTokenSource[] ctss = ParallelInstruction.GenerateCancellationTokenSources(2);
+                    ct.Register(() => ParallelInstruction.CancelAll(ctss));
+                    progress();
+                    
                     Future<TResult1> future1 = null;
                     Future<TResult2> future2 = null;
-                    ct.Register(() =>
+                    Task[] tasks = new[]
                         {
-                            cts1.Cancel();
-                            cts2.Cancel();
-                        });
+                            ParallelInstruction.RunInstruction(instruction1, progress, arg, ctss[0], ctss,
+                                                                cancellationExceptions.Enqueue,
+                                                                operationalExceptions.Enqueue, f => future1 = f),
+                            ParallelInstruction.RunInstruction(instruction2, progress, arg, ctss[1], ctss,
+                                                                cancellationExceptions.Enqueue,
+                                                                operationalExceptions.Enqueue, f => future2 = f)
+                        };
                     progress();
-                    Task task1 = Task.Factory.StartNew(() =>
-                        {
-                            progress();
-                            try
-                            {
-                                future1 = instruction1.InvokeAndWait(cts1.Token, progress, arg);
-                            }
-                            catch (AggregateException e)
-                            {
-                                if (e.InnerException is TaskCanceledException)
-                                    cancellationExceptions.Enqueue(e.InnerException as TaskCanceledException);
-                                else
-                                    operationalExceptions.Enqueue(e.InnerException);
-                                cts2.Cancel();
-                            }
-                            finally
-                            {
-                                progress();
-                            }
-                        });
-                    progress();
-                    Task task2 = Task.Factory.StartNew(() =>
-                        {
-                            progress();
-                            try
-                            {
-                                future2 = instruction2.InvokeAndWait(cts2.Token, progress, arg);
-                            }
-                            catch (AggregateException e)
-                            {
-                                if (e.InnerException is TaskCanceledException)
-                                    cancellationExceptions.Enqueue(e.InnerException as TaskCanceledException);
-                                else
-                                    operationalExceptions.Enqueue(e.InnerException);
-                                cts1.Cancel();
-                            }
-                            finally
-                            {
-                                progress();
-                            }
-                        });
-                    progress();
-                    Task[] tasks = new []{task1, task2};
+                    
                     //should not throw
                     Task.WaitAll(tasks);
                     progress();
 
-                    if (operationalExceptions.Any())
-                        throw operationalExceptions.First();
-                    if (cancellationExceptions.Any())
-                    {
-                        if (ct.IsCancellationRequested)
-                            ct.ThrowIfCancellationRequested();
-                        else
-                            throw cancellationExceptions.First();
-                    }
+                    ParallelInstruction.ProcessParallelExecutionErrors(operationalExceptions, cancellationExceptions, ct);
 
                     //if previous line did not throw, this won't either
                     return new Tuple<TResult1, TResult2>(future1.Result, future2.Result);
